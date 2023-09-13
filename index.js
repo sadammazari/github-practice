ea63b486f0d6e3c34ac078680c0e1ae282724da7
sadammazari <sadamhussainmazari383@gmail.com>
2023-09-12 23:47:31 -0700

    nawazmazari

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+let personname = "khan";
+let age = 23;
+let boolean = true;
+console.log(personname, age, boolean);
---
+++ index.ts
@@ -0,0 +1,4 @@
+let personname:string="khan";
+let age:number=23;
+let boolean=true;
+console.log(personname,age,boolean);
